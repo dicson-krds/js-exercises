2e1109adf6a1798f0462b6f8cad5e50d000a63f1
dicson.deepak <dicson.deepak@krds.fr>
2019-05-29 15:55:02 +0530

    code fine tune

--- 03-dom-manipulation/main.js
+++ 03-dom-manipulation/main.js
@@ -2,51 +2,52 @@
 const classifiedColumn = document.querySelectorAll('.classifiedColumn'),
   pocket = document.getElementById('pocket'),
   pocketLength = pocket.querySelectorAll("li").length,
-  pocketData = document.querySelector('.pocket ul');
-
-console.log(pocketLength);
+  pocketData = document.querySelector('#pocket ul');
 
 const handler = (event) => {
   let data = event.target.innerHTML.charAt(0)
   if (event.target.nodeName === 'LI') {
-    // switch (data) {
-    //   case 'A':
-    //     classifiedColumn[0].appendChild(event.target)
-    //     break;
-    //   case 'B':
-    //     classifiedColumn[1].appendChild(event.target)
-    //     break;
-    //   case 'C':
-    //     classifiedColumn[2].appendChild(event.target)
-    //     break;
-    //   default:
-    //   // code block
+    switch (data) {
+      case 'A':
+        classifiedColumn[0].appendChild(event.target)
+        break;
+      case 'B':
+        classifiedColumn[1].appendChild(event.target)
+        break;
+      case 'C':
+        classifiedColumn[2].appendChild(event.target)
+        break;
+      default:
+      // code block
+    }
+    // if (data === 'A') {
+    //   classifiedColumn[0].appendChild(event.target)
     // }
-    if (data === 'A') {
-      classifiedColumn[0].appendChild(event.target)
-    }
-    if (data === 'B') {
-      classifiedColumn[1].appendChild(event.target)
-    }
-    if (data === 'C') {
-      classifiedColumn[2].appendChild(event.target)
-    }
-    for (let i = 0; i < pocketLength; i++) {
+    // if (data === 'B') {
+    //   classifiedColumn[1].appendChild(event.target)
+    // }
+    // if (data === 'C') {
+    //   classifiedColumn[2].appendChild(event.target)
+    // }
+    for (let i = 0; i < 3; i++) {
       let list = classifiedColumn[i];
+      console.log(list);
       Array.from(list.querySelectorAll("li:not(:first-child)"))
         .sort((a, b) => {
-          if (a.innerHTML > b.innerHTML) return 1
-          else return -1
-        })
-        .forEach(li => list.appendChild(li));
+          if (a.innerHTML > b.innerHTML)
+            return 1
+          else
+            return -1
+        }).forEach(li => list.appendChild(li));
     }
 
-    if (pocketData.children.length === 0) {
-      pocket.parentNode.removeChild(pocket)
+    console.log(pocketData.childElementCount);
+
+    if (pocketData.childElementCount === 0) {
+      pocket.remove();
     }
   }
 
 }
 
 pocket.addEventListener('click', handler);
-
