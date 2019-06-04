c6422a3a38bd99ae4d600f9d78674ca87443ad6b
dicson.deepak <dicson.deepak@krds.fr>
2019-06-04 18:08:26 +0530

    first init

--- 04-color-matching-game/main.js
+++ 04-color-matching-game/main.js
@@ -1,94 +1,79 @@
 //Color matching game
-/*
 const model = {
-  uniqueColors: ['red', 'green', 'yellow', 'cornflowerblue', 'blue', 'purple', 'cyan', 'magenta'],
-  colorBox: [],
-  selectedIndex: null,
-  chosenColor: 0,
+  randomColors: ['red', 'green', 'blue', 'tomato', 'orange', 'gray', 'violet', 'brown'],
+  tile: document.querySelectorAll('#board > li'),
+  tileColors: [],
+  currentIndex: '',
+  previousIndex: '',
   steps: 1,
-  getColorBox: () => model.colorBox,
-  getselectedIndex: () => model.selectedIndex,
-  getsteps: () => model.steps,
-  createColorBox: () => {
-      model.colorBox = model.uniqueColors.map(item => [item, item]).flat()
-      .sort(() => Math.random() - 0.5)
-  },
-  checkChosenColor: () => model.uniqueColors.length === model.chosenColor,
-  updateChosenColor: () => model.chosenColor++,
-  updateSelectedIndex: id => model.selectedIndex = id,
+  gamechecker: 1,
+  currentColor: '',
+  previousColor: '',
   incrementSteps: () => model.steps++,
 }
-
 const view = {
   board: document.querySelector('.board'),
-  cards: document.querySelectorAll('.board li'),
+  tileBoard: document.querySelectorAll('.board li'),
+  steps: document.querySelector('#steps'),
   congratsMsg: document.querySelector('#congrats'),
-  steps: document.querySelector('.steps'),
-  renderTile: (index, color) => {
-      view.cards[index].setAttribute('style', `background-color:${color};pointer-events: none`)
-  },
-  freezeBoard: status => {
-      status ?
-      board.style.pointerEvents = 'none' :
-      board.style.pointerEvents = 'auto'
-  },
-  freezeTiles: (arr) => {
-      arr.forEach(index => view.cards[index].style.pointerEvents = 'none')
-  },
-  resetTiles: (arr) => {
-      arr.forEach(index => view.cards[index].removeAttribute('style'))
-  },
   updateSteps: count => {
-      steps.innerHTML = steps.innerHTML.replace(/[0-9]/g, '') + count;
+    steps.innerHTML = steps.innerHTML.replace(/[0-9]/g, '') + count;
+  },
+  colorMatching: () => {
+    if (model.previousColor) {
+      if (model.currentColor == model.previousColor) {
+        model.tile[model.currentIndex].style.pointerEvents = 'none';
+        model.tile[model.previousIndex].style.pointerEvents = 'none';
+        view.gameCompleted();
+      } else {
+        model.tile[model.currentIndex].setAttribute('style', '');
+        model.tile[model.previousIndex].setAttribute('style', '');
+      }
+      model.previousIndex = '';
+    }
+    else {
+      model.previousIndex = model.currentIndex;
+      model.tile[model.currentIndex].style.pointerEvents = 'none';
+    }
   },
   gameCompleted: () => {
+    if (model.gamechecker == model.randomColors.length) {
       view.congratsMsg.removeAttribute('style');
-      view.board.removeEventListener('click', view.triggerAction)
+      view.board.removeEventListener('click', controller.clickHandler)
+    }
+
+    ++model.gamechecker;
   },
   init: () => {
-      view.board.addEventListener('click', controller.triggerAction)
-  },
-}
-const controller = {
-  board: document.querySelector('.board'),
-  cards: document.querySelectorAll('.board li'),
-  triggerAction: e => {
-      if(e.target.tagName !== 'LI') return false;
-      view.updateSteps(model.incrementSteps())
-
-      const index = Array.from(controller.cards).indexOf(e.target);
-      const previousIndex = model.getselectedIndex();
-      view.renderTile(index, model.getColorBox()[index])
-
-      if(previousIndex == null) {
-          model.updateSelectedIndex(index)
-      }
-      else {
-          controller.compareTiles(previousIndex, index)
-      }
-  },
-  compareTiles: (previousIndex, index) => {
-      const colorBox = model.getColorBox()
-
-      if(colorBox[previousIndex] === colorBox[index]) {
-          view.freezeTiles([previousIndex, index])
-          model.updateChosenColor()
-          model.updateSelectedIndex(null)
-          if(model.checkChosenColor()) {
-              view.gameCompleted()
-          }
-      }
-      else {
-          view.freezeBoard(true)
-          setTimeout(()=> {
-              view.resetTiles([previousIndex, index])
-              model.updateSelectedIndex(null)
-              view.freezeBoard(false)
-          }, 600)
-      }
+    controller.shuffle();
+    view.board.addEventListener('click', controller.clickHandler);
   }
 }
 
-model.createColorBox()
-view.init()
-*/
+const controller = {
+  setTilesColor: () => {
+    controller.shuffle();
+    for (let i = 0; i < model.tileColors.length; i++) {
+      model.tile[i].style.backgroundColor = model.tileColors[i];
+    }
+  },
+  shuffle: () => {
+    model.tileColors = model.randomColors.concat(model.randomColors);
+    model.tileColors = model.tileColors.sort(function () {
+      return .5 - Math.random();
+    });
+    console.log(model.tileColors);
+  },
+  clickHandler: (e) => {
+    if (e.target.tagName !== 'LI') return false;
+    view.updateSteps(model.incrementSteps())
+    model.currentIndex = Array.from(model.tile).indexOf(e.target)
+    model.currentColor = model.tileColors[model.currentIndex]
+    model.previousColor = model.tileColors[model.previousIndex]
+
+    e.target.style.backgroundColor = model.currentColor;
+    view.colorMatching();
+  }
+}
+
+view.init();
