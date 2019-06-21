42580bfd8592f7f572396074c9718792cc246468
dicson.deepak <dicson.deepak@krds.fr>
2019-06-21 17:36:19 +0530

    mvc removed

--- 06-api-handling/main.js
+++ 06-api-handling/main.js
@@ -1,80 +1,92 @@
-const model = {
-  postData: [],
-  postdataUrl: 'https://jsonplaceholder.typicode.com/posts',
-  btnAuthor: null,
-  authordataUrl: 'https://jsonplaceholder.typicode.com/users/'
-}
-const view = {
-  postId: document.getElementById('post'),
-  postDataHandler: () => {
-    data = fetch(model.postdataUrl)
-      .then((data) => data.json())
-      .then((data) => {
-        if (data)
-          return data.map(function (data) {
-            let postData = '<div class="col s4"><div><h4>' + `${data.title}` + '</h4>' + '<p>' + `${data.body}` + '</p><div><a class="waves-effect waves-light btn modal-trigger" href="JavaScript:void(0);" id="' + `${data.id}` + '">Author Details</a><div class="progress"><div class="indeterminate"></div></div></div></div></div>';
-            view.postId.innerHTML += postData;
-          })
-      }).then(() => {
-        model.btnAuthor = document.querySelectorAll('.btn');
-        model.btnAuthor = Array.from(model.btnAuthor)
-        model.btnAuthor.forEach(el => {
-          el.addEventListener('click', controller.authorDataHandler);
-        });
-      }).catch(() => {
-        console.log('Post data Server Down');
-      });
-  },
-  resetPopup: () => {
-    controller.modelCnt.innerHTML = '';
-  },
-  init: () => {
-    view.postDataHandler();
-  }
-}
+let postData = [],
+  postdataUrl = 'https://jsonplaceholder.typicode.com/posts',
+  postList = document.querySelector('#postList');
 
-const controller = {
-  modelCnt: '',
-  model: document.querySelector('.modal'),
-  authorDataHandler: (data) => {
-    view.resetPopup();
-    let btnActive = event.target;
-    btnActive.classList.add('active');
-    tempUrl = model.authordataUrl.concat(data.target.id);
-    modelData = fetch(tempUrl)
-      .then((modelData) => modelData.json())
-      .then((modelData) => {
-        if (modelData)
-          controller.authorPopup(modelData)
-        else
-          controller.authornoData()
+const modal = document.querySelector('.modal'),
+  modalContent = document.querySelector('.modal-content');
 
-        btnActive.classList.remove('active');
-      }).catch(() => {
-        console.log('Author Details Server Down');
-        btnActive.classList.remove('active');
-        controller.authornoData();
-      });
-  },
-  authorPopup: (data) => {
-    controller.modelCnt = document.querySelector('.modal-content');
-    let modelHTML = '<h5><b>Name: </b>' + `${data.name}` + '</h5><h5><b>Email ID: </b>' + `${data.email}` + '</h5><div><div><h6>Company:</h6><p>' + `${data.company.name}` + '<br>' + `${data.company.catchPhrase}` + '<br>' + `${data.company.bs}` + '<br><b>Phone:- </b>' + `${data.phone}` + '<br><b>Website:- </b><a href="https://' + `${data.website}` + '" target="_blank">' + `${data.website}` + '</a></p></div><div><h6>Address:</h6><p>' + `${data.address.street}` + ', ' + `${data.address.suite}` + '<br/>' + `${data.address.city}` + '<br><b>zipcode:- </b>' + `${data.address.zipcode}` + '<br><b>Lat:-</b> ' + `${data.address.geo.lat}` + ', <b>lng:- </b>' + `${data.address.geo.lng}` + '</p></div></div>',
-      instances = M.Modal.init(controller.model, {
-        onCloseEnd: function () {
-          instances.destroy();
-        }
-      });
-    controller.modelCnt.innerHTML = modelHTML;
-    instances.open();
-  },
-  authornoData: () => {
-    let instances = M.Modal.init(controller.model, {
-      onCloseEnd: function () {
-        instances.destroy();
-      }
+function init() {
+  fetch(postdataUrl)
+    .then(res => res.json())
+    .then(res => {
+      renderpostData(res);
+    })
+    .catch(() => {
+      console.log('Post data faild');
     });
-    controller.modelCnt.innerHTML = '<div class="no-data">:( No data From Server</div>';
-    instances.open();
-  }
 }
-view.init();
+
+let postElement = (div) => {
+  div = document.createElement('div');
+  div.setAttribute('class', 'col s4');
+  div.innerHTML = '<div><h4 class="postTitle"></h4><p class="postBody"></p><div><a class="waves-effect waves-light btn postCTA" href="javascript:void(0);">About Author</a><div class="progress"><div class="indeterminate"></div></div></div></div>'
+  return div
+}
+
+const renderpostData = (data) => {
+  let fragment = document.createDocumentFragment();
+
+  Array.from(data).forEach(e => {
+    const html = postElement().cloneNode(true)
+    html.querySelector('.postTitle').innerHTML = e.title
+    html.querySelector('.postBody').innerHTML = e.body
+    html.querySelector('.postCTA').setAttribute('data-id', e.id)
+    fragment.appendChild(html)
+  });
+  postList.appendChild(fragment);
+  postList.addEventListener('click', fetchAuthor);
+}
+
+const renderauthorData = (data) => {
+  modalContent.querySelector('.data').style.display = 'block';
+  modalContent.querySelector('.no-data').style.display = 'none';
+  modalContent.querySelector('.name').innerHTML = data.name;
+  modalContent.querySelector('.email').innerHTML = data.email;
+  modalContent.querySelector('.phone').innerHTML = data.phone;
+  modalContent.querySelector('.website').innerHTML = data.website;
+  modalContent.querySelector('.website').href = 'http://' + data.website;
+  modalContent.querySelector('.zipcode').innerHTML = data.address.zipcode;
+  modalContent.querySelector('.lat').innerHTML = data.address.geo.lat;
+  modalContent.querySelector('.lng').innerHTML = data.address.geo.lng;
+  modalContent.querySelector('.company').innerHTML = data.company.name + '<br>' + data.company.catchPhrase + '<br>' + data.company.bs;
+  modalContent.querySelector('.address').innerHTML = data.address.street + '<br>' + data.address.suite + ', ' + data.address.city;
+
+  const instance = M.Modal.init(modal, {
+    onCloseEnd: function () {
+      instance.destroy();
+    }
+  })
+  instance.open()
+}
+
+const fetchAuthor = (res) => {
+  let _target = res.target;
+  if (_target.tagName !== 'A') return false;
+
+  let user_id = _target.getAttribute('data-id');
+  _target.classList.add('active');
+  fetch(`https://jsonplaceholder.typicode.com/users/${user_id}`)
+    .then(res => res.json())
+    .then(res => {
+      _target.classList.remove('active');
+      renderauthorData(res);
+    })
+    .catch(() => {
+      authornoData();
+      _target.classList.remove('active');
+      console.log('Post data faild');
+    });
+}
+
+const authornoData = () => {
+  const instance = M.Modal.init(modal, {
+    onCloseEnd: function () {
+      instance.destroy();
+    }
+  })
+  instance.open()
+  modalContent.querySelector('.data').style.display = 'none';
+  modalContent.querySelector('.no-data').style.display = 'block';
+  instance.open();
+}
+init();
